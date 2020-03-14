1988cea7fb1cb6565245c8de504a55a30d1df24b
geralldtit <geralldtit@gmail.com>
2020-03-14 17:30:04 +0300

    feat: implement highlighting portfolio items for images

--- script.js
+++ script.js
@@ -3,29 +3,19 @@ const tags = document.getElementById('tags');
 const sendBtn = document.getElementById('send-btn');
 const closeBtn = document.getElementById('close-btn');
 const form = document.getElementById('quote-form');
+const portfolioItems = document.getElementById('items');
 
+/*============================== Header ==============================*/
 menu.addEventListener('click', (event) => {
   menu.querySelectorAll('li').forEach(element => element.classList.remove('active'));
   event.target.parentNode.classList.add('active');
 });
 
+/*============================== Porfolio ==============================*/
 tags.addEventListener('click', (event) => {
   tags.querySelectorAll('li').forEach(element => element.classList.remove('active'));
   event.target.parentNode.classList.add('active');
   swapTwoElements();
-});
-
-sendBtn.addEventListener('click', () => {
-  const subject = document.getElementById('subject').value.toString();
-  const describe = document.getElementById('describe').value.toString();
-  document.getElementById('result-subject').innerText = subject == "" ? "Без темы" : "Тема: " + subject;
-  document.getElementById('result-describe').innerText = describe == "" ? "Без описания" : "Описание: " + describe;
-  document.getElementById('message-block').classList.remove('hidden');
-});
-
-closeBtn.addEventListener('click', (event) => {
-  document.getElementById('result-subject').innerText = document.getElementById('result-describe').innerText = "";
-  document.getElementById('message-block').classList.add('hidden');
 });
 
 function swapTwoElements(){
@@ -38,3 +28,22 @@ function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
+
+portfolioItems.addEventListener('click', (event) => {
+  portfolioItems.querySelectorAll('.item').forEach(item => item.classList.remove('selected-item'));
+  event.target.closest('.item').classList.add('selected-item');
+})
+
+/*============================== Quote ==============================*/
+sendBtn.addEventListener('click', () => {
+  const subject = document.getElementById('subject').value.toString();
+  const describe = document.getElementById('describe').value.toString();
+  document.getElementById('result-subject').innerText = subject == "" ? "Без темы" : "Тема: " + subject;
+  document.getElementById('result-describe').innerText = describe == "" ? "Без описания" : "Описание: " + describe;
+  document.getElementById('message-block').classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', (event) => {
+  document.getElementById('result-subject').innerText = document.getElementById('result-describe').innerText = "";
+  document.getElementById('message-block').classList.add('hidden');
+});

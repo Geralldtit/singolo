687d18fc832d5594956aa8412b7fca37226d4bba
geralldtit <geralldtit@gmail.com>
2020-03-14 16:46:28 +0300

    feat: implement Portfolio active tabs and randomize swap for images

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById('menu');
+const tags = document.getElementById('tags');
 const sendBtn = document.getElementById('send-btn');
 const closeBtn = document.getElementById('close-btn');
 const form = document.getElementById('quote-form');
@@ -6,6 +7,12 @@ const form = document.getElementById('quote-form');
 menu.addEventListener('click', (event) => {
   menu.querySelectorAll('li').forEach(element => element.classList.remove('active'));
   event.target.parentNode.classList.add('active');
+});
+
+tags.addEventListener('click', (event) => {
+  tags.querySelectorAll('li').forEach(element => element.classList.remove('active'));
+  event.target.parentNode.classList.add('active');
+  swapTwoElements();
 });
 
 sendBtn.addEventListener('click', () => {
@@ -20,3 +27,14 @@ closeBtn.addEventListener('click', (event) => {
   document.getElementById('result-subject').innerText = document.getElementById('result-describe').innerText = "";
   document.getElementById('message-block').classList.add('hidden');
 });
+
+function swapTwoElements(){
+  let items = document.getElementsByClassName('item');
+  let randomElement = randomInteger(0, items.length);
+  items[randomElement].parentNode.insertBefore(items[randomElement], items[0]);
+}
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}

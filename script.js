0bc54074aab6ae32a94584c754456f50e8daa382
GeralldTit <geralldtit@gmail.com>
2020-03-15 22:30:07 +0300

    feat: implement infinity carusel

--- script.js
+++ script.js
@@ -4,12 +4,56 @@ const sendBtn = document.getElementById('send-btn');
 const closeBtn = document.getElementById('close-btn');
 const form = document.getElementById('quote-form');
 const portfolioItems = document.getElementById('items');
+const prevBtn = document.getElementById('btn-prev');
+const nextBtn = document.getElementById('btn-next');
 
 /*============================== Header ==============================*/
 menu.addEventListener('click', (event) => {
   menu.querySelectorAll('li').forEach(element => element.classList.remove('active'));
   event.target.closest('li').classList.add('active');
 });
+
+/*============================== Slider ================================*/
+prevBtn.addEventListener('click', () =>{
+  let counter = 0;
+  let interval = setInterval(() => {
+    document.querySelectorAll('.slide').forEach( (item) => {
+      if(item.style.left === '-1020px'){
+        clearInterval(interval);
+        swapTwoSlides('');
+        return;
+      }else {
+        counter -= 10;
+        item.style.left = `${counter}px`;
+      }
+    });
+  }, 10);
+});
+
+nextBtn.addEventListener('click', () =>{
+   swapTwoSlides('-1020px');
+  let counter = -1020;
+  let interval = setInterval(() => {
+    document.querySelectorAll('.slide').forEach( (item) => {
+      if(item.style.left === '0px'){
+        clearInterval(interval);
+        return;
+      }else {
+        counter += 10;
+        item.style.left = `${counter}px`;
+      }
+    });
+  }, 10);
+});
+
+function swapTwoSlides(offset){
+  let slides = document.querySelectorAll('.slide');
+  slides.forEach( (item) => {item.style.left='0px';})
+  slides[1].parentNode.insertBefore(slides[1], slides[0]);
+  if(offset != '') {
+    slides.forEach( (item) => {item.style.left=offset;})
+  }
+}
 
 /*============================== Porfolio ==============================*/
 tags.addEventListener('click', (event) => {
@@ -37,9 +81,9 @@ portfolioItems.addEventListener('click', (event) => {
 /*============================== Quote ==============================*/
 
 sendBtn.addEventListener('click', () => {
-  if(document.getElementById('inputName').checkValidity() &&
-      document.getElementById('inputEmail').checkValidity()) {
-        const subject = document.getElementById('inputSubject').value.toString();
+  if(document.getElementById('name').checkValidity() &&
+      document.getElementById('email').checkValidity()) {
+        const subject = document.getElementById('subject').value.toString();
         const describe = document.getElementById('describe').value.toString();
         document.getElementById('result-subject').innerText = subject == "" ? "Без темы" : "Тема: " + subject;
         document.getElementById('result-describe').innerText = describe == "" ? "Без описания" : "Описание: " + describe;
